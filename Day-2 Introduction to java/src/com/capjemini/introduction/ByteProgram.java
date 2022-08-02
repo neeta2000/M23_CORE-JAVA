7f0ba14796c6b963cb571ba4be52fdf800d0ddd1
hrushikesh palave <hrushikesh palave@DESKTOP-4GAMNJ3>
2022-08-03 00:14:55 +0800

    second commit

---
+++ Day-2 Introduction to java/src/com/capjemini/introduction/ByteProgram.java
@@ -0,0 +1,12 @@
+package com.capjemini.introduction;
+//program for byte datatype
+public class ByteProgram {
+
+	public static void main(String[] args) {
+		byte a=120;         //range(-127 to 128)
+		short b=-20;        //range()
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
