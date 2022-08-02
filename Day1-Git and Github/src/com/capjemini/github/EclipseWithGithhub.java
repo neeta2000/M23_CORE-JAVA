29ef18d122e67e9529e3622f556757cc7ecc0d21
hrushikesh palave <hrushikesh palave@DESKTOP-4GAMNJ3>
2022-08-02 10:56:50 +0800

    First Commit

---
+++ Day1-Git and Github/src/com/capjemini/github/EclipseWithGithhub.java
@@ -0,0 +1,10 @@
+package com.capjemini.github;
+//demo for to connect eclipse with github
+public class EclipseWithGithhub {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to capjemini C2TC pogram");
+
+	}
+
+}
