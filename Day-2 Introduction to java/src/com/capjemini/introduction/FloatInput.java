098d6fb9fa8558e15b8288215a3c4952407e9ca0
hrushikesh palave <hrushikesh palave@DESKTOP-4GAMNJ3>
2022-08-03 17:52:38 +0800

    second commit

--- Day-2 Introduction to java/src/com/capjemini/introduction/FloatInput.java
+++ Day-2 Introduction to java/src/com/capjemini/introduction/FloatInput.java
@@ -7,7 +7,7 @@ public class FloatInput {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		System.out.print("Enter the value of x:-");
-		Float x=s.nextFloat();
+		float x=s.nextFloat();
 		System.out.println("Value of x:-"+x);
 		//Long input
 		/*Scanner s=new Scanner(System.in);
