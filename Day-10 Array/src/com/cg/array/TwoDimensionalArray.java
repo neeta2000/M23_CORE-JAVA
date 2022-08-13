5e20ad7e9b20189f1f5450caeca2d78c0fee568f
hrushikesh palave <hrushikesh palave@DESKTOP-4GAMNJ3>
2022-08-13 10:25:42 +0800

    ten commit

---
+++ Day-10 Array/src/com/cg/array/TwoDimensionalArray.java
@@ -0,0 +1,12 @@
+package com.cg.array;
+//two dimentional array=>arrays of array
+public class TwoDimensionalArray {
+
+	public static void main(String[] args) {
+		int arr[][]= {{11,22,33},{10,20,30}};
+		System.out.println(arr[0][1]);
+		System.out.println(arr[1][1]);
+
+	}
+
+}
